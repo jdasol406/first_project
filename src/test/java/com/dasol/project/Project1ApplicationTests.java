4c599d266626f3a099942f9f19b60f3deae9c75e
전다솔 <jeondasol@jeondasols-MacBook-Air.local>
2023-11-01 00:03:23 +0900

    20231101 first commit

---
+++ src/test/java/com/dasol/project/Project1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dasol.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
